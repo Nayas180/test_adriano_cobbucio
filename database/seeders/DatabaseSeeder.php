58cb6ecbfef31f340d66879002087601cef04fbc
nayas_oliveira <nayasoliveira180@gmail.com>
2025-07-11 03:32:13 -0300

    feat: seeder solicitacoes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Solicitacoes;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Solicitacoes::factory()
+                    ->count(4)
+                    ->sequence(
+                        ["solicitacao" => "Estorno Deposito"],
+                        ["solicitacao" => "Estorno Transferencia"],
+                        ["solicitacao" => "Problema na Transferencia"],
+                        ["solicitacao" => "Problema no Deposito"]
+                    )
+                    ->create();
     }
 }
